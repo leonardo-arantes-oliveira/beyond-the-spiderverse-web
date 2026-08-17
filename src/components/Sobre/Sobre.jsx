dd20074e0ad59b445a7a5aa703252b7332ead77d
dev_leo <146463507+lxzzt@users.noreply.github.com>
2026-08-17 02:22:32 -0300

    v0.0.6.1
    Fix:refatoração no sobre.jsx,mas não está funcional

--- src/components/Sobre/Sobre.jsx
+++ src/components/Sobre/Sobre.jsx
@@ -1,10 +1,10 @@
-import React, { forwardRef } from 'react'
+import React from 'react'
 import style from './Sobre.module.css'
 
-const Sobre = forwardRef(({ data }, ref) => {
+const Sobre = ({ data, refs }) => {
 return (
-    <div ref={ref.container} className={style.sobre}>
-    <h2 ref={ref.titulo} className={style.titulo}>
+    <div ref={refs.container} className={style.sobre}>
+    <h2 ref={refs.titulo} className={style.titulo}>
         {data.desenvolvedor.split('').map((char, index) => (
         <span key={index} style={{ display: 'inline-block', opacity: 0 }}>
             {char === ' ' ? '\u00A0' : char}
@@ -20,14 +20,14 @@ return (
             <li key={contato.id} className={style.item}>
             <a href={contato.link} target="_blank" rel="noreferrer">
                 <span
-                ref={(el) => (ref.imagens.current[index] = el)}
+                ref={(el) => (refs.imagens.current[index] = el)}
                 className={style.iconeWrapper}
                 style={{ opacity: 0 }}
                 >
                 <Icone className={style.icone} />
                 </span>
 
-                <p ref={(el) => (ref.frases.current[index] = el)}>
+                <p ref={(el) => (refs.frases.current[index] = el)}>
                 {contato.text.split('').map((char, charIdx) => (
                     <span key={charIdx} style={{ opacity: 0.2 }}>
                     {char === ' ' ? '\u00A0' : char}
@@ -41,6 +41,6 @@ return (
     </ul>
     </div>
 )
-})
+}
 
 export default Sobre
